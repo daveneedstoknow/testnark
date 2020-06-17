24527a14f7a7abf260ca620395b5e1e6444bafac
dave.hounslow <dave.hounslow@transficc.com>
2020-06-17 17:24:47 +0100

    Only store last build processed not list of builds

--- notifyOnFail.sh
+++ notifyOnFail.sh
@@ -9,14 +9,14 @@ for ARGUMENT in "$@"
 do
 
     KEY=$(echo $ARGUMENT | cut -f1 -d=)
-    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)
     echo $KEY
     case "$KEY" in
             user)              user=${VALUE} ;;
             job)    job=${VALUE} ;;
-            uri)    uri=${VALUE} ;;          
-            *)   
-    esac    
+            uri)    uri=${VALUE} ;;
+            *)
+    esac
 
 
 done
@@ -34,17 +34,15 @@ do
 	lastBuild=$(curl -s "${uri}/job/${job}/lastCompletedBuild/api/json")
 	status=$(echo $lastBuild | jq -r '.result')
 	buildNumber=$(echo $lastBuild | jq -r '.id')
-	if [[ $buildNumbers =~ "${buildNumber}" ]]; then 
-		continue 
+	if [[ $previousBuildNumber =~ "${buildNumber}" ]]; then
+		continue
 	fi
 
-	buildNumbers="$buildNumbers $buildNumber"
-	echo $buildNumbers
+	previousBuildNumber=$buildNumber
+	echo $previousBuildNumber
 	wasMe=$(echo $lastBuild | jq -e ".changeSet.items[].authorEmail | match(\"${user}\")" > /dev/null && echo 0 || echo 1)
 	if [ $status != "SUCCESS" ] && [ $wasMe -eq 0 ]; then
 		notify-send -u critical "$job" "Build #${buildNumber} FAILED\ryou are a possible culprit\r${uri}/job/${job}/lastCompletedBuild/testReport"
 	fi;
 
 done
-
-
